180d628f66c4463dcf8de095e72bc61691318f75
Dilmurod Azimov <dazimo1@wgu.edu>
2026-04-24 14:46:03 -0700

    implement redis-backed rate limiting

--- src/middleware/util/rateLimiter.ts
+++ src/middleware/util/rateLimiter.ts
@@ -1,41 +1,49 @@
 import { rateLimit } from 'express-rate-limit'
+import RedisStore from 'rate-limit-redis'
+
+import { redis } from '../../config/redis'
 import { TIMESPAN } from '../../constants/time'
 import { RateLimitError } from '../../errors'
-import MESSAGES from '../../constants/messages'
-
-// https://www.npmjs.com/package/express-rate-limit
-// TODO - Configure rate limiter in .env
+import { MESSAGES } from '../../constants/messages'
 
 // Default/general rate limit
 const rateLimiter = rateLimit({
+  store: new RedisStore({
+    sendCommand: (...args: string[]): Promise<unknown> => redis.call(...args)
+  }),
   windowMs: TIMESPAN.MINUTE * 10,
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-  standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-  legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+  limit: 100,
+  standardHeaders: 'draft-7',
+  legacyHeaders: false,
   handler: () => {
     throw new RateLimitError(MESSAGES.TOO_MANY_REQUESTS_TRY_AGAIN_IN_10)
-  },
-  // store: ... , // Redis, Memcached, etc. - for rate limiting across distributed systems when you scale.
+  }
 })
 
 const rateLimitOncePerTenMins = rateLimit({
+  store: new RedisStore({
+    sendCommand: (...args: string[]): Promise<unknown> => redis.call(...args)
+  }),
   windowMs: TIMESPAN.MINUTE * 10,
   limit: 1,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
   handler: () => {
     throw new RateLimitError(MESSAGES.TOO_MANY_REQUESTS_TRY_AGAIN_IN_10)
-  },
+  }
 })
 
 const rateLimitTenPerTenMins = rateLimit({
+  store: new RedisStore({
+    sendCommand: (...args: string[]): Promise<unknown> => redis.call(...args)
+  }),
   windowMs: TIMESPAN.MINUTE * 10,
   limit: 10,
   standardHeaders: 'draft-7',
   legacyHeaders: false,
   handler: () => {
     throw new RateLimitError(MESSAGES.TOO_MANY_REQUESTS_TRY_AGAIN_IN_10)
-  },
+  }
 })
 
 export default rateLimiter
